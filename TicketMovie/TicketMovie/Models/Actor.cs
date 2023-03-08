a4a0df7e34170ced2b4f002b695502336f09c113
I-Nhut Nguyen <nguyenvannhut0704@gmail.com>
2023-03-08 15:09:44 +0700

    add Actor

---
+++ TicketMovie/TicketMovie/Models/Actor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketMovie.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProfilePictureUrl { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+    }
+}

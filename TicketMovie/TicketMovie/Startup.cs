f7e2951a299810da9db8dccc3d1609a8251762fb
I-Nhut Nguyen <nguyenvannhut0704@gmail.com>
2023-03-08 15:24:57 +0700

    Add dbContext

--- TicketMovie/TicketMovie/Startup.cs
+++ TicketMovie/TicketMovie/Startup.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TicketMovie.Data;
 
 namespace TicketMovie
 {
@@ -23,6 +24,8 @@ namespace TicketMovie
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //DbContext configuration
+            services.AddDbContext<ApplicationDbContext>();
             services.AddControllersWithViews();
         }
 

9b04f25af0b88f8a45f220a2cf90c115e2133e9d
I-Nhut Nguyen <nguyenvannhut0704@gmail.com>
2023-03-08 15:42:05 +0700

    update dbcontext

--- TicketMovie/TicketMovie/Data/ApplicationDbContext.cs
+++ TicketMovie/TicketMovie/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TicketMovie.Models;
 
 namespace TicketMovie.Data
 {
@@ -13,5 +14,22 @@ namespace TicketMovie.Data
         {
 
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor_Movie>().HasKey(x => new
+            {
+                x.ActorId,
+                x.MovieId
+            });
+            modelBuilder.Entity<Actor_Movie>().HasOne(x => x.Movie).WithMany(y => y.Actor_Movies).HasForeignKey(m =>
+                m.MovieId);
+            modelBuilder.Entity<Actor_Movie>().HasOne(x => x.Actor).WithMany(y => y.Actor_Movies).HasForeignKey(m =>
+                m.ActorId);
+        }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor_Movie> Actor_Movies { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Producer> Producers { get; set; }
     }
 }

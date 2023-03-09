2afdf57116c2fdb26bd3e96074706e311309e90e
I-Nhut Nguyen <nguyenvannhut0704@gmail.com>
2023-03-09 08:34:15 +0700

    add controller

---
+++ TicketMovie/TicketMovie/Controllers/CinemasController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TicketMovie.Data;
+
+namespace TicketMovie.Controllers
+{
+    public class CinemasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CinemasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var dataCinemas = await _context.Actors.ToListAsync();
+            return View();
+        }
+    }
+}
---
+++ TicketMovie/TicketMovie/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TicketMovie.Data;
+
+namespace TicketMovie.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var dataProducers = await _context.Actors.ToListAsync();
+            return View();
+        }
+    }
+}

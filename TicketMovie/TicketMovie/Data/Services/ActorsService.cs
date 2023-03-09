7482b33fcefbb1b8b11ece9428b0564a571d9809
I-Nhut Nguyen <nguyenvannhut0704@gmail.com>
2023-03-09 20:35:31 +0700

    add service

---
+++ TicketMovie/TicketMovie/Data/Services/ActorsService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TicketMovie.Models;
+
+namespace TicketMovie.Data.Services
+{
+    public class ActorsService : IActorsService
+    {
+        private readonly ApplicationDbContext _context;
+        public ActorsService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public void Add(Actor actor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Actor> GetAll()
+        {
+            var result = _context.Actors.ToList();
+            return result;    
+        }
+
+        public Actor GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Actor Update(int id, Actor newActor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
